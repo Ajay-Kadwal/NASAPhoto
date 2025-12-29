d53ec0155c4eb3b889497fff0ee7f72e2f4ae156
Ajay Kadwal <ajaykadwal.work@gmail.com>
2025-12-29 15:38:16 +0530

    Initial Commit

---
+++ NASAPhoto/NASAPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAPhotoApp.swift
+//  NASAPhoto
+//
+//  Created by AJAY KADWAL on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NASAPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
